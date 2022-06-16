c2af61340cf3f47069668a0690721d9e8d9a0ecd
梦影幻忆 <dreamrl@live.com>
2022-06-16 13:10:53 +0800

    add
    new

---
+++ Source/PDFdemo/PDFdemoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PDFdemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PDFDEMO_API APDFdemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
